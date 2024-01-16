8309172d4f2cd018af4127e7a84856e83701636b
Prashant Tomar <141930652+pt8399979@users.noreply.github.com>
2024-01-16 22:46:39 +0530

    Add files via upload

---
+++ AsP2.js
@@ -0,0 +1,28 @@
+// console.log(a);
+// var a = 10;
+
+
+
+
+
+// console.log(a);
+// var a = 10;
+
+// const b = 20;
+// function print(){
+//     console.log("HELLO");
+// }
+// console.log(print);
+// console.log("WORLD");
+
+
+console.log(a);
+var a = 10;
+
+const b = 20;
+function print(){
+    console.log("HELLO");
+}
+console.log(print);
+console.log("WORLD");
+print()
